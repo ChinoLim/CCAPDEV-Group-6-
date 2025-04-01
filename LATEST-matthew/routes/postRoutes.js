73859d386ff3695f2baad7751281bc928b7a6a23
Matt17s <124890759+Matt17s@users.noreply.github.com>
2025-04-02 07:43:24 +0800

    Delete LATEST-matthew directory

--- LATEST-matthew/routes/postRoutes.js
+++
@@ -1,8 +0,0 @@
-
-const express = require('express');
-const router = express.Router();
-const postController = require('../controller/postController');
-
-router.get('/', postController.getRecentPortfolios);
-
-module.exports = router;
